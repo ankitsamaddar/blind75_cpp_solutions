64371ff0f3f7d6c3ec3f1f4f15f665f9d543f5a9
ankit <ankitsam0602@gmail.com>
2023-03-26 22:03:20 +0530

    cpp/hello: create & write

---
+++ cpp/hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+
+int main()
+{
+    std::cout<<"Type your name"<<'\n';
+    std::string name;
+    std::cin>> name;
+    std::cout<<"Hello "<<name<<'\n';
+    return 0;
+}
