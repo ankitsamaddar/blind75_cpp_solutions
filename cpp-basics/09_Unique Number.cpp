91a1d9cadb17658d91d6cb7f2bff72ba866c5ef3
ankit <ankitsam0602@gmail.com>
2023-06-28 04:30:54 +0530

    program 09: update

--- cpp-basics/09_Unique Number.cpp
+++ cpp-basics/09_Unique Number.cpp
@@ -1,7 +1,6 @@
 // DATE: 28-06-2023
 /* PROGRAM: 09_Unique Number
 Given a list of numbers where every numbers occurs twice except one, find that unique number.
-
 INPUT
 7
 3 2 3 5 6 5 2
@@ -9,6 +8,10 @@ INPUT
 OUTPUT
 6
 
+EXPLANATION
+a ^ a = 0
+b ^ b = 0
+a ^ b ^ 6 ^ b ^ a = 6
 */
 // @ankitsamaddar @2023
 #include <iostream>
