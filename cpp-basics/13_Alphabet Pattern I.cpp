ceb6e3c697895099366028cf85dbc39d040dcf86
ankit <ankitsam0602@gmail.com>
2023-06-29 01:22:53 +0530

    program 13: create

--- cpp-basics/13_Alphabet Pattern I.cpp
+++ cpp-basics/13_Alphabet Pattern I.cpp
@@ -1,19 +1,44 @@
 // DATE: 29-06-2023
-/* PROGRAM: 13_Alphabet Pattern I 
+/* PROGRAM: 13_Alphabet Pattern I
+Print the following pattern where for given N = 5, the pattern is.
+ABCDE
+ABCD
+ABC
+AB
+A
 
-INPUT 
+INPUT
+8
+OUTPUT
+ABCDEFG
+ABCDEF
+ABCDE
+ABCD
+ABC
+AB
+A
 
-OUTPUT 
+EXPLANATION
+- incrementing a character goes to the next character in
+*/
+// @ankitsamaddar @2023
+#include <iostream>
+using namespace std;
+void printPattern(int n){
+	for (int i =1; i<=n; i++) {
+		int cnt_=n-i;
+		char alphabet='A';
+		for(int j = 1;j<=cnt_;j++){
+			cout<<alphabet;
+			alphabet++;
+		}
+		cout<<endl;
+	}
+}
 
-EXPLANATION 
-
-*/ 
-// @ankitsamaddar @2023 
-#include <iostream> 
-using namespace std; 
-int main() { 
-    int n; 
-    cin >> n; 
-    cout << "Hello, World" << endl; 
-    return 0; 
-} 
+int main() {
+    int n;
+    cin >> n;
+    printPattern(n);
+	return 0;
+}
