0a09f26de9de38b02f88cd86722d64a1207decd1
ankit <ankitsam0602@gmail.com>
2023-07-05 02:57:28 +0530

    program 33: update

--- cpp-basics/33_String Tokenization using strtok().cpp
+++ cpp-basics/33_String Tokenization using strtok().cpp
@@ -1,16 +1,21 @@
 // DATE: 05-07-2023
 /* PROGRAM: 33_String Tokenization using strtok()
-Input a string and delimeter. Tokenize and print the words using a strtok() function.
+Input a string. Tokenize and print the words using strtok() function.
 INPUT
+See,we are the aliens,Alas !
 
 OUTPUT
+See
+we are the aliens
+Alas !
 
 EXPLANATION
 - <cstring> library function inherited from C
 - char *strtok(char *str, const char *delim) breaks string str into a series of tokens using the delimiter delim.
-- returns a token on each subsequent call. On the first call function should be passed with string argument for 's' and on subsequent calls we should pass the string argument as NULL.
--  NULL indicates we are using the same pointer we used previously
-- char* word will be NULL when there is no more tokens
+- If delimiter is found, it is replaced by '\0' and the pointer to the following character is stored in a static location for subsequent invocations. The function returns the pointer to the beginning of the token.
+
+- returns a token on each subsequent call. On the first function call pass 's' for string argument and for subsequent calls we should pass the string argument as NULL, which indicates we are using the same pointer we used previously.
+- char* word will be NULL when there is no more tokens.
 */
 // @ankitsamaddar @2023
 #include <iostream>
