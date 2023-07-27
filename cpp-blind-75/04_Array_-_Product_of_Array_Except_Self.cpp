25ef7169570132161c5b86a9fbe1738cac23a6e1
ankit <ankitsam0602@gmail.com>
2023-07-27 23:51:55 +0530

    add Product of Array Except Self

---
+++ cpp-blind-75/04_Array_-_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,56 @@
+// DATE: 27-07-2023
+/* PROGRAM: 04_Array - Product of Array Except Self
+
+https://leetcode.com/problems/product-of-array-except-self/
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of
+all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+*/
+// @ankitsamaddar @2023
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+	vector<int> productExceptSelf(vector<int> &nums) {
+		vector<int> res(nums.size());
+		int n = nums.size();
+		int prefix = 1, postfix = 1;
+		for (int i = 0; i < n; i++) {
+			res[i] = prefix;
+			prefix *= nums[i];
+		}
+		for (int i = n - 1; i >= 0; i--) {
+			res[i] *= postfix;
+			postfix *= nums[i];
+		}
+		return res;
+	}
+};
+
+int main() {
+
+	int nums[] = {1, 2, 3, 4};
+	vector<int> v(nums, nums + sizeof(nums) / sizeof(int));
+	Solution sol;
+	vector<int> rem = sol.productExceptSelf(v);
+
+	for (int val : rem) {
+		cout << val << " ";
+	}
+	return 0;
+}
--- cpp-blind-75/README.md
+++ cpp-blind-75/README.md
@@ -9,3 +9,4 @@ Files with suffix `_std` uses _input.txt_ and _output.txt_ as STDIN and STDOUT (
 | 01 | [Array - Two Sum](<01_Array_-_Two_Sum.cpp>) | 27-07-2023 | 
 | 02 | [Array -  Best Time to Buy and Sell Stock](<02_Array_-__Best_Time_to_Buy_and_Sell_Stock.cpp>) | 27-07-2023 | 
 | 03 | [Array - Contains Duplicate](<03_Array_-_Contains_Duplicate.cpp>) | 27-07-2023 | 
+| 04 | [Array - Product of Array Except Self](<04_Array_-_Product_of_Array_Except_Self.cpp>) | 27-07-2023 | 
