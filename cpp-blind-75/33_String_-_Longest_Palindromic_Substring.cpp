b4ba345334c4921588691eb675df97508af14f16
ankit <ankitsam0602@gmail.com>
2023-08-05 22:27:55 +0530

    add: Longest Palindrome Substring

---
+++ cpp-blind-75/33_String_-_Longest_Palindromic_Substring.cpp
@@ -0,0 +1,52 @@
+// DATE: 05-Aug-2023
+/* PROGRAM: 33_String - Longest Palindromic Substring
+
+https://leetcode.com/problems/longest-palindromic-substring/
+
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+
+*/
+// @ankitsamaddar @Aug_2023
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+	public:
+	string longestPalindrome(string s) {
+		string res;
+		for (int i = 0; i < s.length(); i++) {
+			string s1 = palindrome(s, i, i);      // odd length sub-string
+			string s2 = palindrome(s, i, i + 1);  // even length sub-string
+												  // check till last element if even length
+												  // sub-string
+			res = res.length() > s1.length() ? res : s1;
+			res = res.length() > s2.length() ? res : s2;
+		}
+		return res;
+	}
+
+	string palindrome(string s, int l, int r) {
+		while (l >= 0 && r < s.length() && s[l] == s[r]) {
+			l--;
+			r++;
+		}
+		return s.substr(l + 1, r - l - 1);
+	}
+};
+
+int main() {
+	string s = "babad";
+	Solution sol;
+	cout << sol.longestPalindrome(s);
+	return 0;
+}
--- cpp-blind-75/README.md
+++ cpp-blind-75/README.md
@@ -38,3 +38,4 @@ Files with suffix `_std` uses _input.txt_ and _output.txt_ as STDIN and STDOUT (
 | 30 | [String - Group Anagrams](<30_String_-_Group_Anagrams.cpp>) | 05-Aug-2023 | 
 | 31 | [String - Valid Parentheses](<31_String_-_Valid_Parentheses.cpp>) | 05-Aug-2023 | 
 | 32 | [String - Valid Palindrome](<32_String_-_Valid_Palindrome.cpp>) | 05-Aug-2023 | 
+| 33 | [String - Longest Palindromic Substring](<33_String_-_Longest_Palindromic_Substring.cpp>) | 05-Aug-2023 | 
